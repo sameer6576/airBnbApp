ee98b1dbafe052a03c785a73a12049cff69ef741
sameer6576 <77893868+sameer6576@users.noreply.github.com>
2026-06-20 15:37:13 +0530

    setup schema and entity relationship

--- src/main/java/com/sameerahmed/projects/airBnbApp/entity/Payment.java
+++ src/main/java/com/sameerahmed/projects/airBnbApp/entity/Payment.java
@@ -4,8 +4,12 @@ import com.sameerahmed.projects.airBnbApp.entity.enums.PaymentStatus;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 @Entity
 @Getter
 @Setter
@@ -25,6 +29,11 @@ public class Payment {
     @Column(nullable = false, precision = 10, scale = 2)
     private BigDecimal amount;
 
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
 
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
 
 }
